6bd6de0bbe705d5b6f22a8ff61294082b7787b11
unknown <david.willock@hotmail.com>
2018-01-15 18:00:19 +0000

    first commit

---
+++ src/main/java/com/charlie1/StatsApp/App.java
@@ -0,0 +1,18 @@
+package com.charlie1.StatsApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        
+        
+        
+        
+    }
+}
